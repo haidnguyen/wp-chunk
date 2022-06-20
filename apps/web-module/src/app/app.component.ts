eb8b7f35e62c011510f39a6fdc2d24766fd88fd1
Nguyen Dinh Hai <haind@anymindgroup.com>
2022-06-20 15:19:14 +0700

    feat(web-module): add web-module application

---
+++ apps/web-module/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wp-chunk-root',
+  template: ` <wp-chunk-nx-welcome></wp-chunk-nx-welcome> `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'web-module';
+}
